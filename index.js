a95451121bc4730bff4ff9a6d38549b499cccd97
Vincenzo Filosa <vincenzopaolo0@gmail.com>
2020-11-16 14:05:06 +0100

    Various changes

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const Discord = require('discord.js');               //This is needed to include the discord.js library in our main file
-const {prefix, token} = require('./config.json');    //Here we define the bot prefix and the token that are located in the config.json file
+//This will include the discord.js library in our main file
+const Discord = require('discord.js');
 
-const client = new Discord.Client();                 //This string creates a new Discord client
+//Here we are importing our bot prefix and token that we put in the config.json file
+const { prefix, token } = require('./config.json');
+
+//Now we create a new Discord client
+const client = new Discord.Client();
 
 //This is the "ready event", this event executes once when you run the bot, it's needed to actually "turn the bot on"
-client.once('ready', ready => {
-    console.log('The bot\'s ready!');    //This will print in the command prompt the following text as we run the bot to show that the bot is up and functional
+client.once('ready', () => {
+    //The console.log method is used to print text into the console, in this case we are printing this text as soon as the bot is up and running
+    console.log('The bot\'s ready!');
 });
 
-/* Here we have the "message event", this is always active. It is needed to run commands on Discord.
-For example if you want to make a command that responds "pasta" when you type "pizza", you'll have to create that command in this event */
+//The "message event" will run each time a message is sent in a channel, because of that if we'd put "client.once" the event will only run once
 client.on('message', message => {
-    /* In this case there is an if statement, it says that *if the content of the message sent by an user is "prefix ping", send the message "pong".*
-    Where "prefix" indicates the bot prefix located in the config file */
-    if (message.content === prefix+'ping') {
+    //It's always good to check if a user is a bot user or not to prevent our bot from responding to other bots
+    if (message.author.bot) return;
+
+    //If a user sends a message containing the following, the bot will respond with "pong." 
+    if (message.content == `${prefix}ping`) {
         message.channel.send('pong.');
-    } 
-    /* And this is another example where we use exactly the same code as before, the only different thing is the else statement.
-    If and else are always put together and else must always come after if, even though else is not actually needed in this code, it's basically used for other things where it can be useful */
-    else if (message.content === prefix+'beep') {
+    }
+    //Same case as before but with different conditions (the "else" means that if the condition above isn't true, THEN it will check this one)
+    else if (message.content == `${prefix}beep`) {
         message.channel.send('boop.');
     }
-    /* Of course you can put plenty more methods than just the message.channel.send one, there are many methods and properties to execute different actions.
-    You can find all the discord.js methods and properties at https://discord.js.org/#/docs/main/stable/general/welcome */
+    //You can find the official Discord.js documentation at https://discord.js.org/#/docs/main/stable/general/welcome
 });
 
-//finally, we log in to Discord with the bot token that we put in the config.json file
+//Finally, we log in to Discord with the bot token that we put in the config.json file
 client.login(token);
