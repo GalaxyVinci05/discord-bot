918c09e05f3ce704f7d12fab579a96fce1e92c28
Vincenzo Filosa <vincenzopaolo0@gmail.com>
2020-11-17 10:05:25 +0100

    Added event files

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+//ready event
+module.exports = (client) => {
+    //Here we set the bot's rich presence
+    client.user.setPresence({
+        activity: {
+            name: `${client.config.prefix}help on ${client.guilds.cache.size} servers`,
+        },
+        status: 'online'
+    });
+    //Logging into the console when the bot is up and running
+    console.log('The bot\'s ready!');
+};
