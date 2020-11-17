ddcb052344b9523d3a1a0c01d6e9c01b5b0c60ad
Vincenzo Filosa <vincenzopaolo0@gmail.com>
2020-11-17 10:33:04 +0100

    Edited file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: '!',
-    token: 'your-token',
+    token: 'your-token', //If you are hosting your bot on Glitch, put your token in the .env file and put process.env.<VARIABLE_NAME> in this variable
     ownerID: 'your-user-ID'
 };
