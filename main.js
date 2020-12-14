d6b88d0cedbf0d6266ebbc5f68097237da090fe9
Vincenzo Filosa <vincenzopaolo0@gmail.com>
2020-12-14 21:08:20 +0100

    Added dotenv config

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
-//Including the required libraries for our main file
+// Including the required libraries for our main file
 const Discord = require('discord.js');
 const fs = require('fs');
 
-//Here we are defining our config to access the variables in the config.js file
+//
+require('dotenv').config();
+
+// Here we are defining our config to access the variables in the config.js file
 const config = require('./config.js');
 
-//Now we create a new Discord client
+// Now we create a new Discord client
 const client = new Discord.Client();
 
-//This will allow us to access our config file from everywhere
+// This will allow us to access our config file from everywhere
 client.config = config;
 //Simply the installed discord.js version
 client.version = Discord.version;
 
 //- Event handler -
-//For the event and command handler, we use the fs module which allows to read files and directories inside the project
-//Filtering files that end with ".js" which are in the ./events/ directory
+// For the event and command handler, we use the fs module which allows to read files and directories inside the project
+// Filtering files that end with ".js" which are in the ./events/ directory
 const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
 
-//For each event file:
+// For each event file:
 eventFiles.forEach(file => {
-    //Name of the file
+    // Name of the file
     const fileName = `./events/${file}`;
-    //Event name: the file name until the "."
+    // Event name: the file name until the "."
     const eventName = file.split('.')[0];
-    //Getting the event by the fileName
+    // Getting the event by the fileName
     const event = require(fileName);
-    //Loading the events by their name and binding the properties specified in the event file
+    // Loading the events by their name and binding the properties specified in the event file
     client.on(eventName, event.bind(null, client));
     console.log(`Loading ${file.length} events.`);
 });
 
-//Creating new collection for the commands
+// Creating new collection for the commands
 client.commands = new Discord.Collection();
 
-//- Command handler -
-//Filtering files that end with ".js" which are in the ./commands/ directory
+// - Command handler -
+// Filtering files that end with ".js" which are in the ./commands/ directory
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-//For each command file:
+// For each command file:
 commandFiles.forEach(file => {
-    //Name of the file
+    // Name of the file
     const fileName = `./commands/${file}`;
-    //Command name: the file name until the "."
+    // Command name: the file name until the "."
     const commandName = file.split('.')[0];
-    //Getting the command by the fileName
+    // Getting the command by the fileName
     const command = require(fileName);
-    //Setting our commands collection
+    // Setting our commands collection
     client.commands.set(commandName, command);
     console.log(`Loading ${file.length} commands.`);
 });
 
-//Finally, we log in to Discord with the bot token that we put in the config.json file
+// Finally, we log in to Discord with the bot token that we put in the config.json file
 client.login(client.config.token);
