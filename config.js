37894fa9fffac4101f95b0a6b826ce98221b5021
Vincenzo Filosa <vincenzopaolo0@gmail.com>
2020-12-14 21:07:18 +0100

    Set token as environment variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: '!',
-    token: 'your-token', //If you are hosting your bot on Glitch, put your token in the .env file and put process.env.<VARIABLE_NAME> in this variable
+    token: process.env.TOKEN, // Note: you may set this basing on your environment variable set for the Bot token in your .env file
     ownerID: 'your-user-ID'
 };
