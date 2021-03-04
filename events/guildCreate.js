4f57a5dc3db5111171855df66d41820327d3d469
BuildTools <vincenzopaolo0@gmail.com>
2021-03-04 10:50:13 +0100

    Added database configuration

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,18 @@
 module.exports = async (client, guild) => {
+    const dbStructure = {
+        guildName: guild.name,
+        prefix: client.config.prefix,
+        muteRole: ''
+    };
+
+    client.db.set(guild.id, dbStructure);
+
     client.user.setPresence({
         activity: {
-            name: `${client.config.prefix}help on ${client.guilds.cache.size} servers`,
+            name: `${client.config.prefix}help | ${client.guilds.cache.size} servers`,
         },
         status: 'online'
     });
+
     console.log(`The bot just joined a new guild! (${guild.name})`);
 };
