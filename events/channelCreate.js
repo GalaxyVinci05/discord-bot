71bf54d8778782f80196588b502d5c4bbe4f10a4
BuildTools <vincenzopaolo0@gmail.com>
2021-03-04 10:48:52 +0100

    Channel Create event

---
+++ events/channelCreate.js
@@ -0,0 +1,7 @@
+module.exports = async (client, channel) => {
+    if (channel.guild.settings.muteRole && channel.guild.roles.cache.get(channel.guild.settings.muteRole)) {
+        channel.createOverwrite(channel.guild.settings.muteRole, {
+            SEND_MESSAGES: false
+        });
+    }
+};
