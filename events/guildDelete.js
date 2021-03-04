7778f36891947c1f879b11d046a31a5659f6465d
BuildTools <vincenzopaolo0@gmail.com>
2021-03-04 10:50:59 +0100

    Added database deletion

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,9 +1,12 @@
 module.exports = async (client, guild) => {
+    client.db.delete(guild.id);
+    
     client.user.setPresence({
         activity: {
-            name: `${client.config.prefix}help on ${client.guilds.cache.size} servers`,
+            name: `${client.config.prefix}help | ${client.guilds.cache.size} servers`,
         },
         status: 'online'
     });
+
     console.log(`The bot left a guild. (${guild.name})`);
 };
