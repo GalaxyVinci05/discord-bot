eef246b3d239e58667a970b2d79fdcc6307df039
BuildTools <vincenzopaolo0@gmail.com>
2021-03-04 10:44:11 +0100

    Edited comments

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     prefix: '!',
-    token: process.env.TOKEN, // Note: you may set this basing on your environment variable set for the Bot token in your .env file
+    token: process.env.TOKEN, // You may set this basing on your environment variable set for the bot token in your .env file
     ownerID: 'your-user-ID',
     defaultEmbedColor: '7289da' // Must be a HEX value
 };
