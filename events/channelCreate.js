e2de86bae019bbc6c0d499b58f6412e9ae6bccb3
BuildTools <vincenzopaolo0@gmail.com>
2021-03-07 15:29:39 +0100

    Fixed bug

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,5 +1,5 @@
 module.exports = async (client, channel) => {
-    if (channel.guild.settings.muteRole && channel.guild.roles.cache.get(channel.guild.settings.muteRole)) {
+    if (channel.guild && (channel.guild.settings.muteRole && channel.guild.roles.cache.get(channel.guild.settings.muteRole))) {
         channel.createOverwrite(channel.guild.settings.muteRole, {
             SEND_MESSAGES: false
         });
